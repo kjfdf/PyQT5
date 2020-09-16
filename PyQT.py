ece20f8e1b71bece50f2fc9a039ff9e0711969c0
PERIPHERAL\SNUH <kjfdf1@gmail.com>
2020-09-16 12:42:41 +0900

    version 1.1

--- PyQT.py
+++ PyQT.py
@@ -8,8 +8,11 @@ class Exam(QWidget):
     def initUI(self):
         btn=QPushButton('입력하세요',self) #버튼을 '입력하세요'로 만듦
         btn.resize(btn.sizeHint())
-        btn.setToolTip()
+        btn.setToolTip('누르는 버튼입니다.') #버튼에 갔다대면 '누르는 버튼입니다'라고 나옮
         btn.move(25,30) #버튼위치조정
+
+        self.setGeometry(300,300,400,500) #창크기 조절, 가로 400, 세로 500, 왼쪽에서 300, 오른쪽에서 300
+
         self.show()
 
 app=QApplication(sys.argv)
